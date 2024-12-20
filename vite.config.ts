c680385d92dca6c043715d9ea41efd90d74df183
owsu23 <owsu23@student.bth.se>
2024-12-20 20:42:01 +0100

    Installed dotenv, added env-file, added gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 1337, // Set your desired port
+    port: parseInt(process.env.PORT || "3000")
   },
 })
