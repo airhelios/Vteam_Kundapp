075bb183ca16b59ecbf5ce6945a2430c852eb555
owsu23 <owsu23@student.bth.se>
2024-12-30 19:57:17 +0100

    Added button for removing all rentals

--- src/helpers/bike-functions.tsx
+++ src/helpers/bike-functions.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { API_URL, getHeader } from '../helpers/config';
 import { toast } from 'react-toastify';
 import { toastOptionsError, toastOptionsSuccess } from '../helpers/config';
@@ -47,7 +47,6 @@ export const bikePerCity = async (city: string, token: string, status = '') : Pr
             }
     }
     catch(error: any) {
-        console.log(error.response?.data);;
         toast.error(error.response.data.message, toastOptionsError)
 
     }
@@ -63,7 +62,6 @@ export const allBikes = async ( token:string ) : Promise<any> =>
                 data = response.data;
         }
         catch(error: any) {
-            console.log(error.response);
             toast.error(error.response.data.message, toastOptionsError)
 
         }
@@ -78,14 +76,11 @@ export const rentBike = async (bikeId: string, token: string) : Promise<RentBike
             const response = await axios.post(`${API_URL}/rental/bike/${bikeId}`, {}, getHeader(token));
 
             data = {... response.data, "statusCode": 200 };
-            console.log(response);
-            console.log(data);
+
                 
         }
         catch(error: any) {
-            console.log(error.response.data.message);
             toast.error(error.response.data.message,toastOptionsError)
-            console.log(error.response.data);
             data = error.response.data;
         }
         return data;
@@ -98,12 +93,9 @@ export const returnBike = async (tripID: string | null, token: string) : Promise
             try {
                     const response = await axios.post(`${API_URL}/rental/${tripID}/end`, {}, getHeader(token));
                     data = {... response.data, "statusCode": 200 };
-                    console.log(response)
-                    console.log(data)
             }
             catch(error: any) {
-                console.log(error)
-                console.log(error.response);
+
                 toast.error(error.response.data.message, toastOptionsError)
                 data = error.response.data;
             }
@@ -111,26 +103,12 @@ export const returnBike = async (tripID: string | null, token: string) : Promise
 }
 
 
-// export const allRentals = async (userId: string, token: string): Promise<any> => {
-//     let data = {};
-//     try {
-//         const response = await axios.get(`${API_URL}/rental/`, getHeader(token));
-//         data = response.data.filter((item: any)  => item.customer.username === userId);
-//     } catch(error)
-//     {
-//         console.log(`No rentals found for user ${userId}`);
-//     }
-//     return data;
-// }
-
 export const allRentals = async (token: string): Promise<any> => {
     let data = {};
     try {
         let me = await axios.get(`${API_URL}/auth/me`, getHeader(token));
         let userId = me.data.githubId;
         const response = await axios.get(`${API_URL}/rental/customer/${userId}`, getHeader(token));
-        console.log(userId)
-        // data = response.data.filter((item: any)  => item.customer.username === userId);
         data = response.data;
     } catch(error)
     {
@@ -145,4 +123,19 @@ export const bikeIdByFive = async (bikeIdFive: string | null, token: string): Pr
     const bike = data.find((item: bikeArray) => item.id.startsWith(bikeIdFive?.toLowerCase()));
 
     return bike.id;
+}
+
+export const returnAllRentals = async (token: string): Promise<any> => {
+    try {
+        let me = await axios.get(`${API_URL}/auth/me`, getHeader(token));
+        let userId = me.data.githubId;
+        const data = await axios.post(`${API_URL}/rental/${userId}/end-all-travels`,{}, getHeader(token));
+        return data
+    } catch(error)
+    {  
+        
+        if (error instanceof AxiosError && error.response?.data?.message) {
+            toast.error(`Rentals were not returned: ${error.response.data.message}`, toastOptionsError);
+        }
+    }
 }
